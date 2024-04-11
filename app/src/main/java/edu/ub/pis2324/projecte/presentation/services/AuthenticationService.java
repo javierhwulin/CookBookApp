7899bdb4bfe4ea0d91544d99e0beae9db5197cc1
xiwei <xiwei187@gmail.com>
2024-04-11 10:30:25 +0200

    init

---
+++ app/src/main/java/edu/ub/pis2324/projecte/presentation/services/AuthenticationService.java
@@ -0,0 +1,11 @@
+package edu.ub.pis2324.projecte.presentation.services;
+
+import edu.ub.pis2324.recyclerandsearchviewexample.data.services.mockdata.MockClientsHashMap;
+import edu.ub.pis2324.recyclerandsearchviewexample.domain.Client;
+
+/**
+ * Cloud Firestore implementation of the data store.
+ */
+public class AuthenticationService {
+
+}
