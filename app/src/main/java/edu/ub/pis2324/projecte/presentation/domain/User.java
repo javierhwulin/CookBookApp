759e4ee9c1b894cfeb6e7d695b011a2f9ad3e1fc
Mastonmo <126565271+Mastonmo@users.noreply.github.com>
2024-04-11 09:56:19 +0200

    User

---
+++ app/src/main/java/edu/ub/pis2324/projecte/presentation/domain/User.java
@@ -0,0 +1,31 @@
+package edu.ub.pis2324.projecte.presentation.domain;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
