680f13a96c3b96c0f89bb51e12f6be3417a14cbf
Mastonmo <126565271+Mastonmo@users.noreply.github.com>
2024-04-11 10:41:48 +0200

    Repos

---
+++ app/src/main/java/edu/ub/pis2324/projecte/domain/User.java
@@ -0,0 +1,33 @@
+package edu.ub.pis2324.projecte.domain;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String username;
+    private String password;
+    private String email;
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+}
