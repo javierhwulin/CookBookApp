7f725134f0aec0d025d3e739183c9fa908758be8
Mastonmo <126565271+Mastonmo@users.noreply.github.com>
2024-04-11 10:06:09 +0200

    ViewModel

--- app/src/main/java/edu/ub/pis2324/projecte/presentation/viewmodel/SignUpViewModel.java
+++ app/src/main/java/edu/ub/pis2324/projecte/presentation/viewmodel/SignUpViewModel.java
@@ -1,6 +1,23 @@
 package edu.ub.pis2324.projecte.presentation.viewmodel;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import edu.ub.pis2324.projecte.presentation.livedata.StateLiveData;
+
 public class SignUpViewModel extends ViewModel {
+
+    private final StateLiveData<Void> signUpState;
+
+    public SignUpViewModel (){
+        super();
+        signUpState = new StateLiveData<>();
+    }
+
+
+    public StateLiveData<Void> getSignUpState (){
+        return signUpState;
+    }
+
+
 }
